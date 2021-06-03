ee82002dd0edba96b4296eae02530840d825c7a6
fishay <44349120+fishay@users.noreply.github.com>
2021-06-02 22:13:20 -0400

    Create 0547NumberofProvinces.py

---
+++ algorithms/medium/0547NumberofProvinces.py
@@ -0,0 +1,45 @@
+There are n cities. Some of them are connected, while some are not. 
+If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+Return the total number of provinces.
+
+Example 1:
+Input: isConnected = [[1,1,0],
+                      [1,1,0],
+                      [0,0,1]]
+Output: 2
+  
+Example 2:
+Input: isConnected = [[1,0,0],
+                      [0,1,0],
+                      [0,0,1]]
+Output: 3
+
+Constraints:
+1 <= n <= 200
+n == isConnected.length
+n == isConnected[i].length
+isConnected[i][j] is 1 or 0.
+isConnected[i][i] == 1
+isConnected[i][j] == isConnected[j][i]
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        seen = [False] * n
+        count = 0
+        
+        def dfs(i):
+            if seen[i]: return
+            seen[i] = True
+            for j in range(n):
+                if i!= j and isConnected[i][j] == 1:
+                    dfs(j)
+        
+        for i in range(n):
+            if seen[i] == False:
+                count += 1
+                dfs(i)
+        
+        return count
